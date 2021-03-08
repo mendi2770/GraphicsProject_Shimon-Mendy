6465ba06f630e1888a29c9838904ac5db1d06bc1
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-03-08 15:20:33 +0200

    starting Point3D class

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -2,8 +2,23 @@ package primitives;
 
 public class Point3D {
 
-	public Point3D() {
-		// TODO Auto-generated constructor stub
+	final static Point3D ZERO = new Point3D(0,0,0);
+	
+	Coordinate x;
+	Coordinate y;
+	Coordinate z;
+	
+
+	public Point3D(Coordinate p1,Coordinate p2,Coordinate p3) {
+		x = p1;
+		y= p2;
+		z= p3;
+	}
+	
+	public Point3D(double p1,double p2,double p3) {
+		x = new Coordinate(p1);
+		y= new Coordinate(p2);
+		z= new Coordinate(p3);
 	}
 
 }
