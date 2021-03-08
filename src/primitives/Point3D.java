fdf5f72cee4453bbceb4725b45ebe715e1a6bdd6
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-03-08 21:40:42 +0200

    finish Point3D class

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -20,5 +20,27 @@ public class Point3D {
 		y= new Coordinate(p2);
 		z= new Coordinate(p3);
 	}
+	
+	public Vector subtract(Point3D p1) {
+	Vector temp = new Vector(p1.x.coord - this.x.coord,p1.y.coord - this.y.coord,p1.z.coord-this.z.coord);
+	return temp;	
+	}
+	
+	public Point3D add(Vector vec) {
+		Point3D temp = new Point3D(vec.head.x.coord + this.x.coord, vec.head.y.coord + this.y.coord, vec.head.z.coord + this.z.coord);
+		return temp;
+	}
+	
+	double distanceSquared(Point3D p1,Point3D p2) {
+		Vector vec = p1.subtract(p2);
+		double distance = vec.head.x.coord * vec.head.x.coord + vec.head.y.coord * vec.head.y.coord +
+				vec.head.z.coord * vec.head.z.coord;
+		return distance;
+	}
+	
+	double distance(Point3D p1,Point3D p2) {
+		double dis = Math.sqrt(distanceSquared(p1, p2));
+		return dis;
+	}
 
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -2,8 +2,17 @@ package primitives;
 
 public class Vector {
 
-	public Vector() {
-		// TODO Auto-generated constructor stub
+	Point3D head;
+	
+	public Vector(Coordinate p1,Coordinate p2,Coordinate p3) {
+		head = new Point3D(p1, p2, p3);
+	}
+	
+	public Vector(double p1,double p2,double p3) {
+		head = new Point3D(p1, p2, p3);
 	}
 
+	public Vector(Point3D p1) {
+		head = p1;
+	}
 }
