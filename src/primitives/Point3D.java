144d25f10ba158d24facb4fdb489824e9538c453
ShimonD9 <diskin@g.jct.ac.il>
2021-03-09 12:48:19 +0200

    Some progress in vector class

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,5 +1,7 @@
 package primitives;
 
+import static primitives.Util.isZero;
+
 public class Point3D {
 
 	final static Point3D ZERO = new Point3D(0,0,0);
@@ -11,14 +13,14 @@ public class Point3D {
 
 	public Point3D(Coordinate p1,Coordinate p2,Coordinate p3) {
 		x = p1;
-		y= p2;
-		z= p3;
+		y = p2;
+		z = p3;
 	}
 	
 	public Point3D(double p1,double p2,double p3) {
 		x = new Coordinate(p1);
-		y= new Coordinate(p2);
-		z= new Coordinate(p3);
+		y = new Coordinate(p2);
+		z = new Coordinate(p3);
 	}
 	
 	public Vector subtract(Point3D p1) {
@@ -42,5 +44,21 @@ public class Point3D {
 		double dis = Math.sqrt(distanceSquared(p1, p2));
 		return dis;
 	}
+	
+	   /*************** Admin *****************/
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point3D)) return false;
+        Point3D other = (Point3D)obj;
+        return isZero(other.x.coord - this.x.coord) && isZero(other.y.coord - this.y.coord) && isZero(other.z.coord - this.z.coord);
+    }
 
+    @Override
+    public String toString() {
+        return "(" + x + " , " + y + " , " + z + ")";
+    }
+    
+    
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,18 +1,70 @@
 package primitives;
 
+import static primitives.Util.isZero;
+
 public class Vector {
 
 	Point3D head;
-	
-	public Vector(Coordinate p1,Coordinate p2,Coordinate p3) {
-		head = new Point3D(p1, p2, p3);
+
+	public Vector(Coordinate p1, Coordinate p2, Coordinate p3) {
+		Point3D p = new Point3D(p1, p2, p3);
+		if (p.equals(p.ZERO))
+			throw new IllegalArgumentException("The coordiates cannot be  zeroes.");
+		head = p;
 	}
-	
-	public Vector(double p1,double p2,double p3) {
-		head = new Point3D(p1, p2, p3);
+
+	public Vector(double p1, double p2, double p3) {
+		Point3D p = new Point3D(p1, p2, p3);
+		if (p.equals(p.ZERO))
+			throw new IllegalArgumentException("The numbers cannot be zeroes.");
+		head = p;
 	}
 
 	public Vector(Point3D p1) {
 		head = p1;
+	}
+
+	public Vector subtract(Vector vec) {
+		Vector temp = new Vector(this.head.x.coord - vec.head.x.coord, this.head.y.coord - vec.head.y.coord, this.head.z.coord - vec.head.z.coord);
+		return temp;
+	}
+
+	public Vector add(Vector vec) {
+		Vector temp = new Vector(this.head.x.coord + vec.head.x.coord, this.head.y.coord + vec.head.y.coord, this.head.z.coord + vec.head.z.coord);		Vector temp = new Vector(vec.head.x.coord + this.head.x.coord, vec.head.y.coord + this.head.y.coord, vec.head.z.coord + this.head.z.coord);
+		return temp;
+	}
+	
+	public Vector scale(double scale) {
+		Vector temp = new Vector(scale * this.head.x.coord, scale * this.head.y.coord, scale * this.head.z.coord);
+		return temp;
+	}
+	
+	public double dotProduct(Vector vec) {
+		return (this.head.x.coord * vec.head.x.coord + this.head.y.coord * vec.head.y.coord + this.head.z.coord * vec.head.z.coord);
+	}
+
+	public Vector crossProduct(Vector vec) {
+		Vector temp = new Vector(this.head.y.coord * vec.head.z.coord - this.head.z.coord * vec.head.y.coord,
+								this.head.z.coord * vec.head.x.coord - this.head.x.coord * vec.head.z.coord,
+								this.head.x.coord * vec.head.y.coord - this.head.y.coord * vec.head.x.coord);				Vector temp = new Vector(vec.head.x.coord + this.head.x.coord, vec.head.y.coord + this.head.y.coord, vec.head.z.coord + this.head.z.coord);
+		return temp;
+	}
+	
+	/*************** Admin *****************/
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Vector))
+			return false;
+		Vector other = (Vector) obj;
+		return other.head.equals(this.head);
+	}
+
+	@Override
+	public String toString() {
+		return this.head.toString();
 	}
 }
