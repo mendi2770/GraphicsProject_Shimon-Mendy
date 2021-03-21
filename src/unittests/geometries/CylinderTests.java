f8ea7f6a3df2ecdca03d56dd7c6539ab1abfb33b
ShimonD9 <diskin@g.jct.ac.il>
2021-03-21 11:57:36 +0200

    Started cylinder test

--- src/unittests/geometries/CylinderTests.java
+++ src/unittests/geometries/CylinderTests.java
@@ -7,6 +7,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import geometries.Cylinder;
+import geometries.Tube;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
 /**
  * @author 97253
  *
@@ -18,7 +24,13 @@ public class CylinderTests {
 	 */
 	@Test
 	public void testGetNormal() {
-		fail("Not yet implemented");
+		Vector vec = new Vector(1, 1, 1);
+		Point3D pt = new Point3D(2, 2, 2);
+		Ray ray = new Ray(vec, pt);
+		Cylinder cyl = new Cylinder(ray, 5, 10);
+		// ============ Equivalence Partitions Tests ==============
+        // TC01: Wrong normal calculation (in case the point is not across the ray.p0)
+		assertEquals("getNormal() - does not work correctly",new Vector(Math.sqrt(1/2d),-1 * Math.sqrt(1/2d),0), cyl.getNormal(new Point3D(12,2,7)));	
 	}
 
 }
