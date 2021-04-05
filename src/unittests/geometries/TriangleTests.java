06ab9052cda90f36bca9dc8e5fe3e8d4ef87fb9f
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-04-05 13:07:57 +0300

    starts triangle tests

--- src/unittests/geometries/TriangleTests.java
+++ src/unittests/geometries/TriangleTests.java
@@ -23,11 +23,37 @@ public class TriangleTests {
 	 */
 	@Test
 	public void testGetNormal() {
-        // ============ Equivalence Partitions Tests ==============
-        // TC01: There is a simple single test here
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: There is a simple single test here
 		Triangle triangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
-        double sqrt3 = Math.sqrt(1d / 3); // Normalizing the vector components
-        assertTrue("Bad normal to plane", new Vector(sqrt3, sqrt3, sqrt3).equals(triangle.getNormal(new Point3D(0, 0, 1))) 
-        		||  new Vector(-1 * sqrt3, -1 * sqrt3, -1 * sqrt3).equals(triangle.getNormal(new Point3D(0, 0, 1))));
+		double sqrt3 = Math.sqrt(1d / 3); // Normalizing the vector components
+		assertTrue("Bad normal to plane",
+				new Vector(sqrt3, sqrt3, sqrt3).equals(triangle.getNormal(new Point3D(0, 0, 1)))
+						|| new Vector(-1 * sqrt3, -1 * sqrt3, -1 * sqrt3)
+								.equals(triangle.getNormal(new Point3D(0, 0, 1))));
+	}
+
+	/**
+	 * Test method for {@link geometries.Polygon#findIntersections}.
+	 */
+	@Test
+	public void testFindIntersections() {
+
+		// ============ Equivalence Partitions Tests ==============
+
+		// TC11: Inside polygon/triangle
+
+		// TC12: Outside against edge
+
+		// TC13: Outside against vertex
+
+		// =============== Boundary Values Tests ==================
+		// ***** (the ray begins "before" the plane)
+
+		// TC21: On edge
+
+		// TC22: In vertex
+
+		// TC23: On edge's continuation
 	}
 }
