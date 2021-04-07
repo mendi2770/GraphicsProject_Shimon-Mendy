e6b888df4c257fc3d76bf1f3a4ae445e5a067c1b
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-04-07 20:56:59 +0300

    starts triangle implementation

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -3,18 +3,22 @@ package geometries;
 import primitives.*;
 import static primitives.Util.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Triangle extends Polygon {
 
 	public Triangle(Point3D a, Point3D b, Point3D c) {
-		
-		super(a, b, c);		
+
+		super(a, b, c);
 	}
 
 	@Override
 	public List<Point3D> findIntersections(Ray ray) {
-		// TODO Auto-generated method stub
+		;
+		List<Point3D> resultPoint = plane.findIntersections(ray);
+
+
 		return null;
 	}
 }
