6ff686ed1f375b5d60072cd20344e75108edf39e
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-04-19 12:51:41 +0300

    starts image tests

--- src/unittests/ImageWriterTest.java
+++ src/unittests/ImageWriterTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import primitives.Color;
+import renderer.ImageWriter;
+
 /**
  * @author shimo
  *
@@ -18,11 +21,16 @@ public class ImageWriterTest {
 	 */
 	@Test
 	public void testWriteToImage() {
-		fail("Not yet implemented");
+		Color color = new Color(java.awt.Color.blue);
+		ImageWriter image = new ImageWriter("imageTest", 16, 10);
+		for (int i = 0; i < 500; i++)
+			for (int j = 0; j < 800; j++) {
+				image.writePixel(j, i, color);
+			}
 	}
-
 	/**
-	 * Test method for {@link renderer.ImageWriter#writePixel(int, int, primitives.Color)}.
+	 * Test method for
+	 * {@link renderer.ImageWriter#writePixel(int, int, primitives.Color)}.
 	 */
 	@Test
 	public void testWritePixel() {
