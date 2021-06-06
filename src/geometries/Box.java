c70e4a58ebf1ab9291df9bee3cad37b1fb55ea46
ShimonD9 <diskin@g.jct.ac.il>
2021-06-06 12:25:26 +0300

    Started Box intersection function

--- src/geometries/Box.java
+++ src/geometries/Box.java
@@ -3,6 +3,8 @@
  */
 package geometries;
 
+import primitives.Ray;
+
 /**
  * @author 97253
  * Box class for every shape
@@ -38,6 +40,63 @@ public class Box {
 		this.minX = minX;
 		this.minY = minY;
 		this.minZ = minZ;
+	}
+	
+	public boolean IsRayHitBox(Ray ray)
+	{
+		// Ray coordinates:
+		double rDx = ray.getDir().getHead().getX();
+		double rDy = ray.getDir().getHead().getY();
+		double rDz = ray.getDir().getHead().getZ();
+		
+		// Ray head point coordinates:
+		double rOx = ray.getP0().getX();
+		double rOy = ray.getP0().getY();
+		double rOz = ray.getP0().getZ();
+		
+		// In case the ray is parallel to one of the dimensions
+		// there is no intersection with the box:
+		if (rDx == 0 && rOx < minX || rOx > maxX)
+			return false;
+		if (rDy == 0 && rOy < minY || rOy > maxY)
+			return false;
+		if (rDz == 0 && rOz < minZ || rOz > maxZ)
+			return false;
+		
+		
+		// Intersection distance t1 and t2 for each dimension:
+		double temp;
+		double t1x = (minX - rOx) / rDx;
+		double t2x = (maxX - rOx) / rDx;
+		if (t1x > t2x)
+		{
+			temp = t1x;
+			t1x = t2x;
+			t2x = temp;
+		}
+			
+		
+		double t1y = (minY - rOy) / rDy;
+		double t2y = (maxY - rOy) / rDy;
+		if (t1y > t2y)
+		{
+			temp = t1y;
+			t1y = t2y;
+			t2y = temp;
+		}
+		
+		double t1z = (minZ - rOz) / rDz;
+		double t2z = (maxZ - rOz) / rDz;
+		if (t1z > t2z)
+		{
+			temp = t1z;
+			t1z = t2z;
+			t2z = temp;
+		}
+
+		
+		
+		return false;
 	}
 	
 	
