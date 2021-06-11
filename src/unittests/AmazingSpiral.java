d4eeda70c953f9c19ce4c6841b8fe853efe9ffcc
ShimonD9 <diskin@g.jct.ac.il>
2021-06-11 09:00:20 +0300

    Trying the triangles

--- src/unittests/AmazingSpiral.java
+++ src/unittests/AmazingSpiral.java
@@ -21,7 +21,7 @@ public class AmazingSpiral {
 			.setDistance(1000).setViewPlaneSize(200, 200);
 	private final Scene scene = new Scene("Test scene");
 
-	private static final Color color = new Color(200, 0, 0);
+	private static final Color color = new Color(192, 192, 192);
 	private static final Material mat = new Material().setKd(0.5).setKs(0.5).setnShininess(60);
 	private static final double radius = 0.7;
 	/**
@@ -31,32 +31,52 @@ public class AmazingSpiral {
 	public void goblet() {
 
 		// Head of the goblet:
-		Point3D center = new Point3D(0, 0, 0);
-		Point3D newPoint;
 		double angle = 0.261; // 15 degree
 		Vector toMoveVector;
 		double oldX = -5;
 		double oldY = 0;
 		double newX;
 		double newY;
-		double radius = 0.2;
+		double newXlevel2;
+		double newYlevel2;
 		int totalRotations = 24; // 15 degree * 24 = 360
+		// Level 1 points:
+		Point3D a;
+		Point3D b;
+		// Level 2 points:
+		Point3D c;
+		Point3D d;
 		for (int j = 0; j > -5; j--) // Depth level
 		{
-			
-			scene.geometries.add(new Sphere(new Point3D(oldX, oldY, j), radius).setEmission(color).setMaterial(mat));
+
+//			scene.geometries.add(new Sphere(new Point3D(oldX, oldY, j), radius).setEmission(color).setMaterial(mat));
 			for (double i = 0; i < totalRotations; i = i + 1) { // Number of angle rotations
+				// Obtaining the points:
+				toMoveVector = new Vector(oldX, oldY, 0);
+				a = new Point3D(0, 0, j).add(toMoveVector);
+				toMoveVector = new Vector(oldX + 1, oldY, 0);
+				c = new Point3D(0, 0, j - 1).add(toMoveVector);
+				// b:
 				newX = Math.cos(angle) * oldX - Math.sin(angle) * oldY;
 				newY = Math.sin(angle) * oldX + Math.cos(angle) * oldY;
-				toMoveVector = new Vector(newX, newY, j);
-				newPoint = center.add(toMoveVector);
-				scene.geometries.add(new Sphere(newPoint, radius).setEmission(color).setMaterial(mat));
+				toMoveVector = new Vector(newX, newY, 0);
+				b = new Point3D(0, 0, j).add(toMoveVector);
+				// d:
+				newXlevel2 = Math.cos(angle) * (oldX + 1) - Math.sin(angle) * oldY;
+				newYlevel2 = Math.sin(angle) * (oldX + 1) + Math.cos(angle) * oldY;
+				toMoveVector = new Vector(newX, newY, 0);
+				d = new Point3D(0, 0, j - 1).add(toMoveVector);
+				
+				
+				
+				// Creating the triangles:
+
+				scene.geometries.add(new Triangle(a, b, c).setEmission(color).setMaterial(mat),
+						new Triangle(c, d, b).setEmission(color).setMaterial(mat));
 				oldX = newX;
 				oldY = newY;
 			}
 			oldX++;
-			radius = radius * 0.8;
-			center = new Point3D(0, 0, j);
 		}
 
 		// Lights and rendering
