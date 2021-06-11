1f62d996f51585061e9fdb79345c4956bb097366
ShimonD9 <diskin@g.jct.ac.il>
2021-06-11 17:49:38 +0300

    Finished head and leg of goblet - need more improvements

--- src/unittests/AmazingSpiral.java
+++ src/unittests/AmazingSpiral.java
@@ -17,68 +17,173 @@ import java.lang.Math;
  * @author Dan
  */
 public class AmazingSpiral {
-	private final Camera camera = new Camera(new Point3D(0, 0, -100), new Vector(0, 0, 1), new Vector(0, 1, 0)) //
+//	private final Camera camera = new Camera(new Point3D(0, 0, -200), new Vector(0, 0, 1), new Vector(0, 1, 0)) //
+//			.setDistance(1000).setViewPlaneSize(200, 200);
+	private final Camera camera = new Camera(new Point3D(-200, 0, 0), new Vector(1, 0, 0), new Vector(0, 1, 0)) //
 			.setDistance(1000).setViewPlaneSize(200, 200);
 	private final Scene scene = new Scene("Test scene");
 
-	private static final Color color = new Color(192, 192, 192);
-	private static final Material mat = new Material().setKd(0.5).setKs(0.5).setnShininess(60);
-	private static final double radius = 0.7;
+	private static final Color color = new Color(200, 0, 0); // Red
+	private static final Color color2 = new Color(255, 215, 0); // Gold
+	private static final Material mat = new Material().setKd(0.5).setKs(0.5).setnShininess(60).setkR(0.4);
+	private static final double radius = 0.2;
+
 	/**
 	 * Produce a scene with a 3D model and render it into a png image
 	 */
 	@Test
 	public void goblet() {
 
-		// Head of the goblet:
 		double angle = 0.261; // 15 degree
+		int totalRotations = 26; // 15 degree * 24 = 360 and two to close the gap
 		Vector toMoveVector;
 		double oldX = -5;
 		double oldY = 0;
 		double newX;
 		double newY;
-		double newXlevel2;
-		double newYlevel2;
-		int totalRotations = 24; // 15 degree * 24 = 360
-		// Level 1 points:
+		int headDepthLevel = -7;
+		int legDepthLevel = -12;
+		int bottomDepthLevel = -15;
+
 		Point3D a;
 		Point3D b;
-		// Level 2 points:
-		Point3D c;
-		Point3D d;
-		for (int j = 0; j > -5; j--) // Depth level
-		{
 
-//			scene.geometries.add(new Sphere(new Point3D(oldX, oldY, j), radius).setEmission(color).setMaterial(mat));
-			for (double i = 0; i < totalRotations; i = i + 1) { // Number of angle rotations
-				// Obtaining the points:
-				toMoveVector = new Vector(oldX, oldY, 0);
-				a = new Point3D(0, 0, j).add(toMoveVector);
-				toMoveVector = new Vector(oldX + 1, oldY, 0);
-				c = new Point3D(0, 0, j - 1).add(toMoveVector);
-				// b:
+		/////////////// Head of the goblet /////////////////////
+		Point3D[] pntsLevel1 = new Point3D[totalRotations];
+		Point3D[] pntsLevel2 = new Point3D[totalRotations];
+		for (int j = 0; j > headDepthLevel; j--) // Depth level
+		{
+			oldX = headDepthLevel - j;
+			oldY = 0;
+
+			// Obtaining the points:
+			for (int i = 0; i < totalRotations; i = i + 1) { // Number of angle rotations
+
+				// Vector rotation and new point creation:
 				newX = Math.cos(angle) * oldX - Math.sin(angle) * oldY;
 				newY = Math.sin(angle) * oldX + Math.cos(angle) * oldY;
 				toMoveVector = new Vector(newX, newY, 0);
-				b = new Point3D(0, 0, j).add(toMoveVector);
-				// d:
-				newXlevel2 = Math.cos(angle) * (oldX + 1) - Math.sin(angle) * oldY;
-				newYlevel2 = Math.sin(angle) * (oldX + 1) + Math.cos(angle) * oldY;
-				toMoveVector = new Vector(newX, newY, 0);
-				d = new Point3D(0, 0, j - 1).add(toMoveVector);
-				
-				
-				
-				// Creating the triangles:
+				a = new Point3D(0, 0, j).add(toMoveVector);
 
-				scene.geometries.add(new Triangle(a, b, c).setEmission(color).setMaterial(mat),
-						new Triangle(c, d, b).setEmission(color).setMaterial(mat));
+				// Adding the new point to the appropriate array:
+				if (j % 2 == 0)
+					pntsLevel1[i] = a;
+				else
+					pntsLevel2[i] = a;
+
+				// Updating to the newest point on the circle:
 				oldX = newX;
 				oldY = newY;
 			}
-			oldX++;
+
+			// Creating the triangles:
+			if (j < 0) {
+				for (int i = 0; i < totalRotations - 1; i++) {
+					if (j%2 == 0)
+					{
+					scene.geometries.add(
+					new Triangle(pntsLevel1[i], pntsLevel1[i + 1], pntsLevel2[i + 1]).setEmission(color).setMaterial(mat),
+					new Triangle(pntsLevel1[i], pntsLevel2[i], pntsLevel2[i + 1]).setEmission(color2)
+							.setMaterial(mat));
+					}
+					else
+					{
+					scene.geometries.add(
+					new Triangle(pntsLevel1[i], pntsLevel1[i + 1], pntsLevel2[i + 1]).setEmission(color2).setMaterial(mat),
+					new Triangle(pntsLevel1[i], pntsLevel2[i], pntsLevel2[i + 1]).setEmission(color)
+							.setMaterial(mat));
+					}
+						
+				}
+			}
 		}
 
+		/////////////// Leg of the goblet /////////////////////
+
+		angle = 0.05235; // 3 degree
+		totalRotations = 122; // 3 degree * 120 = 360 and two to close the gap
+		pntsLevel1 = new Point3D[totalRotations];
+		pntsLevel2 = new Point3D[totalRotations];
+		oldX = -1;
+		oldY = 0;
+		// Obtaining the points:
+		for (int i = 0; i < totalRotations; i = i + 1) { // Number of angle rotations
+															// Vector rotation and new point creation:
+			newX = Math.cos(angle) * oldX - Math.sin(angle) * oldY;
+			newY = Math.sin(angle) * oldX + Math.cos(angle) * oldY;
+			toMoveVector = new Vector(newX, newY, 0);
+			a = new Point3D(0, 0, headDepthLevel + 1).add(toMoveVector);
+			b = new Point3D(0, 0, legDepthLevel).add(toMoveVector);
+			// Adding the new point to the appropriate array:
+			pntsLevel1[i] = a;
+			pntsLevel2[i] = b;
+
+			// Updating to the newest point on the circle:
+			oldX = newX;
+			oldY = newY;
+		}
+
+		// Creating the Polygons:
+		for (int i = 0; i < totalRotations - 1; i++) {
+			scene.geometries.add(
+					new Polygon(pntsLevel1[i], pntsLevel1[i + 1], pntsLevel2[i + 1], pntsLevel2[i]).setEmission(color).setMaterial(mat));
+		}
+
+		/////////////// Bottom of the goblet /////////////////////
+
+		angle = 0.261; // 15 degree
+		totalRotations = 26; // 15 degree * 24 = 360 and two to close the gap
+		pntsLevel1 = new Point3D[totalRotations];
+		pntsLevel2 = new Point3D[totalRotations];
+		oldX = -1;
+		oldY = 0;
+		for (int j = 0; j > bottomDepthLevel; j--) // Depth level
+		{
+			oldX = bottomDepthLevel - j;
+			oldY = 0;
+
+			// Obtaining the points:
+			for (int i = 0; i < totalRotations; i = i + 1) { // Number of angle rotations
+
+				// Vector rotation and new point creation:
+				newX = Math.cos(angle) * oldX - Math.sin(angle) * oldY;
+				newY = Math.sin(angle) * oldX + Math.cos(angle) * oldY;
+				toMoveVector = new Vector(newX, newY, 0);
+				a = new Point3D(0, 0, j).add(toMoveVector);
+
+				// Adding the new point to the appropriate array:
+				if (j % 2 == 0)
+					pntsLevel1[i] = a;
+				else
+					pntsLevel2[i] = a;
+
+				// Updating to the newest point on the circle:
+				oldX = newX;
+				oldY = newY;
+			}
+
+			// Creating the triangles:
+			if (j < 0) {
+				for (int i = 0; i < totalRotations - 1; i++) {
+					if (j%2 == 0)
+					{
+					scene.geometries.add(
+					new Triangle(pntsLevel1[i], pntsLevel1[i + 1], pntsLevel2[i + 1]).setEmission(color).setMaterial(mat),
+					new Triangle(pntsLevel1[i], pntsLevel2[i], pntsLevel2[i + 1]).setEmission(color2)
+							.setMaterial(mat));
+					}
+					else
+					{
+					scene.geometries.add(
+					new Triangle(pntsLevel1[i], pntsLevel1[i + 1], pntsLevel2[i + 1]).setEmission(color2).setMaterial(mat),
+					new Triangle(pntsLevel1[i], pntsLevel2[i], pntsLevel2[i + 1]).setEmission(color)
+							.setMaterial(mat));
+					}
+						
+				}
+			}
+		}
+		
 		// Lights and rendering
 		scene.lights.add(new PointLight(new Color(500, 500, 500), new Point3D(100, 0, -100), 1, 0.0005, 0.0005) //
 				.setkQ(0.000001));
